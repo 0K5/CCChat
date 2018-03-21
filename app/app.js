f89d8c7e536bd53bbc7285ef5031909e3c427464
0K5 <0k5@protonmail.com>
2018-03-21 12:28:46 +0100

    Initialized Session over RedisDatabase

--- app/app.js
+++ app/app.js
@@ -1,17 +1,22 @@
 (() => {
-	let logger = require('./helpers/logger.js');
+    let logger = require('./helpers/logger.js');
     let path = require('path');
     let hostname = 'localhost';
     let sPort = 3000;
     let port = 4000;
     let handlebars = require('express-handlebars');
+
     logger.logInfo('Setting up App');
     let express = require('express')
     let app = express();
     let fs = require('fs');
     let forceSsl = require('express-force-ssl');
+    let bodyParser = require('body-parser');
     app.use(forceSsl);
-    app.use(express.json());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
     logger.logInfo('Setting up Server');
     let https = require('https');
@@ -21,7 +26,6 @@
         key: key,
         cert: cert
     };
-
     let httpsServ = https.createServer(options, app).listen(sPort);
     let http = require('http');
     let httpServ = http.createServer(app).listen(port);
@@ -30,49 +34,59 @@
     let session = require('express-session');
     let redisStore = require('connect-redis')(session);
     let redis = require('redis');
-    let tls = require('tls');
-    let ssl = {
-        key: key,
-        cert: cert
-    };
-    let client = redis.createClient(sPort, hostname, {
-        tls: ssl
+    //TODO: With valid ssl certificate comment in
+    //let tls = require('tls');
+    //let ssl = {
+    //    key: key,
+    //    cert: cert
+    //};
+    let client = redis.createClient(11379,
+        'redis-11379.c11.us-east-1-2.ec2.cloud.redislabs.com', {
+            no_ready_check: true
+            //TODO: With valid ssl certificate replace upper line with:
+            //tls: ssl
+        });
+    client.auth('UUJKCfW3vEClG6Zh6dvE9FfggYtBcYvi', function(err) {
+        if (err) throw err;
     });
-    app.use(session({
-        secret: '14gty8i9oph1q45o;pgh3p0[987oui2dh3q2l4iugfrh',
-        store: new redisStore({
-            host: 'localhost',
-            port: 6479,
-            client,
-            ttl: 260
-        }),
-        saveUninitialized: false,
-        resave: false
-    }));
-	
-    logger.logInfo('Setting up routes');
+    client.on('connect', function() {
+        logger.logInfo('Connected to Redis');
+        app.use(session({
+            secret: '14gty8i9oph1q45o;pgh3p0[987oui2dh3q2l4iugfrh',
+            store: new redisStore({
+                host: 'localhost',
+                port: 6479,
+                client,
+                ttl: 260
+            }),
+            saveUninitialized: false,
+            resave: false
+        }));
 
-    logger.logInfo('Setting up sockets');
-    let io = require('socket.io')(httpsServ);
-    io.on('connection', (socket) => {
-        console.log(socket.handshake.cookies);
-    });
+        logger.logInfo('Setting up routes');
 
-    logger.logInfo('Setting up handlebars');
-    app.engine('hbs', handlebars({
-        extname: 'hbs',
-        defaultLayout: 'main',
-        layoutsDir: __dirname + '/views/layouts/'
-    }));
-    app.set('views', path.join(__dirname, 'views/'));
-    app.set('view engine', 'hbs');
+        logger.logInfo('Setting up sockets');
+        let io = require('socket.io')(httpsServ);
+        io.on('connection', (socket) => {
+            console.log(socket.handshake.cookies);
+        });
 
-    logger.logInfo('Initializing express application');
-    app.use(express.static(path.join(__dirname, '/public')));
-    app.get('/', function(req, res, next) {
-		logger.logDeb('Session: ' + req.session);
-        res.render('index.hbs', {
-            title: 'RPSGame'
+        logger.logInfo('Setting up handlebars');
+        app.engine('hbs', handlebars({
+            extname: 'hbs',
+            defaultLayout: 'main',
+            layoutsDir: __dirname + '/views/layouts/'
+        }));
+        app.set('views', path.join(__dirname, 'views/'));
+        app.set('view engine', 'hbs');
+
+        logger.logInfo('Initializing express application');
+        app.use(express.static(path.join(__dirname, '/public')));
+        app.get('/', function(req, res, next) {
+            logger.logDeb('SessionId: ' + req.session.id);
+            res.render('index.hbs', {
+                title: 'RPSGame'
+            });
         });
     });
 })();
